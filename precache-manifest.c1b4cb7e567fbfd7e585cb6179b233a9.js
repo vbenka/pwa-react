7fab1a1661fc367bf33ee1657e61a6dda287e013
vbenka <vladislav.benka@gmail.com>
2019-09-06 18:23:02 +0200

    Updates

---
+++ precache-manifest.c1b4cb7e567fbfd7e585cb6179b233a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025ce6c903759a6911a2b0ff7cddc8ed",
+    "url": "/pwa-react/index.html"
+  },
+  {
+    "revision": "63ae63154c847af26c87",
+    "url": "/pwa-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1e3b3dd5eaafdb96388f",
+    "url": "/pwa-react/static/js/2.cd97cde6.chunk.js"
+  },
+  {
+    "revision": "63ae63154c847af26c87",
+    "url": "/pwa-react/static/js/main.64f8069a.chunk.js"
+  },
+  {
+    "revision": "862c6e0cfd2f17d4c414",
+    "url": "/pwa-react/static/js/runtime~main.668a1025.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
